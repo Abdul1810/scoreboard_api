8f3fad23c963878bcc38615ed4f8e2b7d5938a49
Abdul1810 <abdulking8010@gmail.com>
2025-02-18 14:12:23 +0530

    Partially completed image streamer with speed calc

--- src/main/java/com/api/ImageStreamer.java
+++ src/main/java/com/api/ImageStreamer.java
@@ -1,29 +1,83 @@
 package com.api;
 
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
+import jakarta.websocket.*;
+import jakarta.websocket.server.ServerEndpoint;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.concurrent.Future;
 
-@WebServlet("/image")
-public class ImageStreamer extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String imagePath = "C:\\Users\\ACER\\Downloads\\52259221868_3d2963c1fe_o.png";
-        File imageFile = new File(imagePath);
+@ServerEndpoint("/imagews")
+public class ImageStreamer {
+    private final static String imagePathSmall = "C:\\Users\\ACER\\Downloads\\testimage\\small.jpg";
+    private final static String imagePathMedium = "C:\\Users\\ACER\\Downloads\\testimage\\medium.jpg";
+    private final static String imagePathBig = "C:\\Users\\ACER\\Downloads\\testimage\\large.png";
 
-        response.setContentType("image/png");
-        response.setContentLengthLong(imageFile.length());
+    private final static int KILOBYTE = 1024;
+    private final static int MEGABYTE = 1024 * KILOBYTE;
 
-        try (BufferedInputStream imageIn = new BufferedInputStream(new FileInputStream(imageFile)); OutputStream out = response.getOutputStream()) {
-            byte[] buffer = new byte[1024];
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("Opened session: " + session.getId());
+        session.setMaxIdleTimeout(10 * 60 * 1000);
+        session.setMaxBinaryMessageBufferSize(1024 * 1024);
+
+        RemoteEndpoint.Async remote = session.getAsyncRemote();
+        remote.sendText("starting");
+        File imageFile = new File(imagePathMedium);
+        double fileSize = imageFile.length();
+
+        try (InputStream imageStream = new FileInputStream(imageFile)) {
+            BufferedInputStream bufferedStream = new BufferedInputStream(imageStream);
+            int chunkSize = 1024 * KILOBYTE;
+//            if (fileSize <= 96 * KILOBYTE) {
+//                chunkSize = 8 * KILOBYTE;
+//            } else if (fileSize <= 384 * KILOBYTE) {
+//                chunkSize = 32 * KILOBYTE;
+//            } else if (fileSize <= 1536 * KILOBYTE) {
+//                chunkSize = 128 * KILOBYTE;
+//            } else if (fileSize <= 5120 * KILOBYTE) {
+//                chunkSize = 512 * KILOBYTE;
+//            } else if (fileSize <= 24576 * KILOBYTE) {
+//                chunkSize = 2048 * KILOBYTE;
+//            } else if (fileSize <= 102400 * KILOBYTE) {
+//                chunkSize = 5120 * KILOBYTE;
+//            } else {
+//                chunkSize = 10240 * KILOBYTE;
+//            }
+            byte[] buffer = new byte[chunkSize];
             int bytesRead;
+            int count = 0;
 
-            while ((bytesRead = imageIn.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesRead);
-                out.flush();
+            while ((bytesRead = bufferedStream.read(buffer)) != -1) {
+                ByteBuffer byteBuffer = ByteBuffer.wrap(buffer, 0, bytesRead);
+                long startTime = System.nanoTime();
+                try {remote.sendBinary(byteBuffer);
+                } catch (Exception e) {
+                    System.out.println("Error sending chunk " + count + ": " + e);
+                }
+                long endTime = System.nanoTime();
+                long timeTaken = endTime - startTime;
+                double diffSeconds = timeTaken / 1_000_000_000.0;
+                System.out.println("Time taken to send chunk " + count + ": " + diffSeconds + " s");
+                chunkSize = (int) (chunkSize / diffSeconds);
+                System.out.println("New chunk size: " + chunkSize);
+                if (chunkSize > 5 * MEGABYTE) {
+                    chunkSize = 5 * MEGABYTE;
+                } else if (chunkSize < MEGABYTE) {
+                    chunkSize = MEGABYTE;
+                }
+                buffer = new byte[chunkSize];
+                count++;
             }
+            System.out.println("image send  done" + count);
+        } catch (Exception e) {
+            System.out.println("Streaming error: " + e);
         }
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        System.out.println("Closed session: " + session.getId());
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,12 +4,81 @@
     <title>Photo Stream</title>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <script>
+        let socket;
+        let chunks = [];
+        let imgElement;
+        let oldBlob;
+
+        document.addEventListener("DOMContentLoaded", function () {
+            socket = new WebSocket("ws://localhost:8080/imagews");
+            imgElement = document.getElementById("result");
+
+            const testConnectionSpeed = {
+                imageAddr: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Brandenburger_Tor_abends.jpg", // this is just an example, you rather want an image hosted on your server
+                downloadSize: 2707459,
+                run: function (mbps_max, cb_gt, cb_lt) {
+                    testConnectionSpeed.mbps_max = parseFloat(mbps_max) ? parseFloat(mbps_max) : 0;
+                    testConnectionSpeed.cb_gt = cb_gt;
+                    testConnectionSpeed.cb_lt = cb_lt;
+                    testConnectionSpeed.InitiateSpeedDetection();
+                },
+                InitiateSpeedDetection: function () {
+                    window.setTimeout(testConnectionSpeed.MeasureConnectionSpeed, 1);
+                },
+                result: function () {
+                    const duration = (endTime - startTime) / 1000;
+                    const bitsLoaded = testConnectionSpeed.downloadSize * 8;
+                    const speedBps = (bitsLoaded / duration).toFixed(2);
+                    const speedKbps = (speedBps / 1024).toFixed(2);
+                    const speedMbps = (speedKbps / 1024).toFixed(2);
+                    if (speedMbps >= (testConnectionSpeed.max_mbps ? testConnectionSpeed.max_mbps : 1)) {
+                        testConnectionSpeed.cb_gt ? testConnectionSpeed.cb_gt(speedMbps) : false;
+                    } else {
+                        testConnectionSpeed.cb_lt ? testConnectionSpeed.cb_lt(speedMbps) : false;
+                    }
+                },
+                MeasureConnectionSpeed: function () {
+                    const download = new Image();
+                    download.onload = function () {
+                        endTime = (new Date()).getTime();
+                        testConnectionSpeed.result();
+                    }
+                    startTime = (new Date()).getTime();
+                    const cacheBuster = "?nnn=" + startTime;
+                    download.src = testConnectionSpeed.imageAddr + cacheBuster;
+                }
+            }
+            // testConnectionSpeed.run(1.5, function (speedMbps) {
+            //     console.log("Connection speed is greater than 1 Mbps: " + speedMbps + " Mbps");
+            // }, function (speedMbps) {
+            //     console.log("Connection speed is less than 1 Mbps: " + speedMbps + " Mbps");
+            // });
+
+            socket.onmessage = function (event) {
+                if (event.data instanceof Blob) {
+                    chunks.push(event.data);
+                    if (oldBlob) {
+                        URL.revokeObjectURL(oldBlob);
+                    }
+                    oldBlob = new Blob(chunks, {type: "image/jpeg"});
+                    imgElement.src = URL.createObjectURL(oldBlob);
+                }
+            };
+
+            socket.onerror = function (error) {
+                console.error("WebSocket Error:", error);
+            };
+
+            socket.onclose = function () {
+                console.log("WebSocket closed.");
+            };
+        });
+    </script>
 </head>
 <body style="text-align: center; font-family: Arial, sans-serif;">
-<h1>Image Stream</h1>
-<p>
-    Java, Servlet, WebSocket, JSP
-</p>
-<img src="/image" alt="image" style="max-width: 100%;"/>
+<h1>Photo Stream</h1>
+<p>Java, WebSocket, JSP, Servlet</p>
+<img id="result" style="max-width: 100%; height: auto; border: 1px solid #ccc;"/>
 </body>
 </html>
